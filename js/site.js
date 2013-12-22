7fb30cc28b49c0c4c2051e227d123b18eaf4c09a
unknown <andy@grafixwerks.com>
2013-12-22 17:42:22 -0500

    added in images and css

---
+++ js/site.js
@@ -0,0 +1,9 @@
+// Andy Pearson
+// CSCI E-60 P final project
+
+$(document).ready(function(){
+	  // tablesorter 
+	  $("table.tablesorter").tablesorter({widgets : ['zebra']});
+	  
+}); // close document ready
+  
