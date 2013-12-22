37f2cf22bcf4a7dfdf3994e983819afd43eefe35
unknown <andy@grafixwerks.com>
2013-12-22 18:22:30 -0500

    reworked tablesorter js to exclude columns

--- js/site.js
+++ js/site.js
@@ -2,8 +2,26 @@
 // CSCI E-60 P final project
 
 $(document).ready(function(){
-	  // tablesorter 
-	  $("table.tablesorter").tablesorter({widgets : ['zebra']});
+	  // homepage tablesorter 
+	  $("table.listing").tablesorter({widgets : ['zebra']});
+
+	  // user dashboard ad list tablesorter 
+	  $("table.your-ads").tablesorter({
+		  widgets : ['zebra'],
+headers: {
+            2: { sorter: false },
+            3: { sorter: false }
+        }		  
+		  });
+
+
+	  // user dashboard messages tablesorter 
+	  $("table.messages").tablesorter({
+		  widgets : ['zebra'],
+headers: {
+            3: { sorter: false }
+        }		  
+		  });
 	  
 }); // close document ready
   
